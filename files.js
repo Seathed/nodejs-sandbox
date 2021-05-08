ebb992eceb08861173e0bc3a14eb257efd6fd856
Liam Hart <liamhart700@gmail.com>
2021-05-08 18:26:29 +0930

    writing files with in-built fs module

--- files.js
+++ files.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
 // reading files
-fs.readFile('./docs/post1.txt', (err, data) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(data.toString());
-});
+// fs.readFile('./docs/post1.txt', (err, data) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log('Reading files is asynchronous')
 
 // writing files
+fs.writeFile('./docs/post2.txt', 'This is the second post in the log.',() => {
+    console.log('file was written')
+});
 
 // directories
 
