8f39a17e5898549b964df5659835755120d5a755
Liam Hart <liamhart700@gmail.com>
2021-05-08 18:28:10 +0930

    creating directories with in-built fs module

--- files.js
+++ files.js
@@ -11,10 +11,16 @@ const fs = require('fs');
 // console.log('Reading files is asynchronous')
 
 // writing files
-fs.writeFile('./docs/post2.txt', 'This is the second post in the log.',() => {
-    console.log('file was written')
-});
+// fs.writeFile('./docs/post2.txt', 'This is the second post in the log.',() => {
+//     console.log('file was written')
+// });
 
 // directories
+fs.mkdir('./assets', (err) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log('directory created');
+});
 
 // deleting files
