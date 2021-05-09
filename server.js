e8fa5f0fb7147aa98adad905ef3e960ae4d8e281
Liam Hart <liamhart700@gmail.com>
2021-05-09 10:01:45 +0930

    using lodash functions

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log('request received');
-    console.log(req.url, req.method);
+    
+    // lodash
+    const num = _.random(0, 25);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
+    greet();
 
     let path = './views/';
 
