57a902ad5c89753642ac7e6d6295a254b7ee273b
Liam Hart <liamhart700@gmail.com>
2021-05-09 11:04:47 +0930

    Sending dynamic content to ejs files

--- app.js
+++ app.js
@@ -12,19 +12,24 @@ const server = app.listen(8282);
 // routing
 app.get('/', (req, res) => {
     // render is used to send an ejs view
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi is a dinosaur', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Waluigi is the best character in the Mario Universe', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to play Mario Kart', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ]
+    res.render('index', {title: 'Home'});
 });
 
 app.get('/home', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a New Blog'});
 });
 
 // redirects
@@ -35,5 +40,5 @@ app.get('/about-me', (req, res) => {
 // 404
 app.use((req, res) => {
     res.statusCode = 404;
-    res.render('404');
+    res.render('404', {title: '404'});
 });
