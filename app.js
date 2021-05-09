9b59530cfc407366f91136a2fd743c8dbb1658d0
Liam Hart <liamhart700@gmail.com>
2021-05-09 10:26:41 +0930

    sending html pages with express

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const server = app.listen(8282);
 
 // routing
 app.get('/', (req, res) => {
-    // infers the type of content sent
-    // infers the headers
-    // infers the status code
-    res.send('<h1>Home Page</h1>');
+    // looks for an absolute path
+    // need a second argument to specify the root directory to search in
+    res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.sendFile('./views/about.html', { root: __dirname });
 });
