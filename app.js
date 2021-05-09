560fc409d4d339bf44e1e6e286d4cee70927ee2c
Liam Hart <liamhart700@gmail.com>
2021-05-09 14:27:25 +0930

    retrieving database collection and displaying it on a page

--- app.js
+++ app.js
@@ -44,26 +44,6 @@ app.get('/add-blog', (req, res) => {
     });
 });
 
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('6097676e2bf76905cc8e6d3f')
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
-
 app.use((req, res, next) => {
     fs.appendFile('./log.txt','Request Made at ' + `${timestamp('YY/MM/DD HH:mm:ss')}` + '\nHost: ' + req.hostname + '\nPath: ' + req.path + '\nMethod: ' + req.method + '\n\n', (err) => {
         if(err) {
@@ -74,16 +54,6 @@ app.use((req, res, next) => {
 });
 
 // routing
-app.get('/', (req, res) => {
-    // render is used to send an ejs view
-    const blogs = [
-        {title: 'Yoshi is a dinosaur', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Waluigi is the best character in the Mario Universe', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to play Mario Kart', snippet: 'Lorem ipsum dolor sit amet consectetur'}
-    ]
-    res.render('index', {title: 'Home', blogs});
-});
-
 app.get('/home', (req, res) => {
     res.render('index', {title: 'Home'});
 });
@@ -97,12 +67,32 @@ app.get('/blogs/create', (req, res) => {
 });
 
 // redirects
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
 app.get('/about-me', (req, res) => {
     res.redirect('/about');
 });
 
-// 404
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find()
+    .sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('blogs/create', (req, res) => {
+
+    // 404
 app.use((req, res) => {
-    res.statusCode = 404;
+     res.statusCode = 404;
     res.render('404', {title: '404'});
+});
+
 });
